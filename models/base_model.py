86580c327b293fe42d6a064c9487deda881abc00
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-08-13 23:17:07 +0000

    Task 7 completed

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,10 @@ class BaseModel:
             *args (tuples): contains arguments.
             **kwargs (dictionary): Contains arguments with key-value pairs
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -24,11 +28,7 @@ class BaseModel:
                             datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
                 elif key != '__class__':
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        storage.new(self)
 
     def __str__(self):
         """
